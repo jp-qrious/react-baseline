08648ead83d748e25ae0661a2c962f15b8b40031
jp-qrious <jp.caparas@qrious.co.nz>
2023-08-18 08:28:20 +1200

    Install other dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(<App greeting="Hello, React with Parcel and TypeScript!" />, document.getElementById('app'));
+ReactDOM.render(<App greeting="Hello, React with Parcel and TypeScripts!" />, document.getElementById('app'));
